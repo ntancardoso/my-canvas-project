79821fdd874bfb49f8da0d5a2d89dffd06995486
ntancardoso <niny0@msn.com>
2024-09-28 14:09:11 +0800

    - Refactor objects
    - Game Object class
    - Hero Class
    - Refactor to the new classes

---
+++ src/GameObject.js
@@ -0,0 +1,41 @@
+import { Vector2 } from "./Vector2";
+
+export class GameObject {
+    constructor({ position }) {
+        this.position = position ?? new Vector2(0, 0);
+        this.children = [];
+    }
+
+    stepEntry(delta, root) {
+        this.children.forEach((child) => child.stepEntry(delta, root));
+
+        this.step(delta, root);
+    }
+
+    step(_delta) {
+
+    }
+
+    draw(ctx, x, y) {
+        const drawPosX = x + this.position.x;
+        const drawPosY = y + this.position.y;
+
+        this.drawImage(ctx, drawPosX, drawPosY);
+
+        this.children.forEach((child) => child.draw(ctx, drawPosX, drawPosY));
+    }
+
+    drawImage(ctx, drawPosX, drawPosY) {
+
+    }
+
+    addChild(gameObject) {
+        this.children.push(gameObject)
+    }
+
+    remoevChild(gameObject) {
+        this.children = this.filter(g => {
+            return gameObject !== g;
+        })
+    }
+}
